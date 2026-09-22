ac19b16c4db95bd166ff6cb8b539899f1a43b701
Nurles2006-lol <nurles9898@gmail.com>
2026-09-22 11:25:52 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main ()
+
+{
+	printf("Name : 이승한\n");
+	printf("Student ID : 202310627\n");
+	printf("Major : AI 빅데이터\n");
+	printf("Determination : C언어를 마스터하고 코딩과 친해지고 싶습니다.\n");
+	return 0;
+}
